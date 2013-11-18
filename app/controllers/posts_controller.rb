bff251a30553bb4f27f7f36ca3a880c12dca44a7
David Schoemehl <davidschoemehl@gmail.com>
2013-11-17 21:10:36 -0600

    Adding the ability to edit and update, along with a partial form
    Up to step 6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   
   def new
+		@post = Post.new
   end
 
   def create
@@ -20,6 +21,27 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
 
 private
     def post_params
